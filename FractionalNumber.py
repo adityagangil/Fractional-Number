d7f25103596f4d0f414733c9e6fe4aa70bb576fe
Aditya Gangil <111217346+adityagangil@users.noreply.github.com>
2023-03-04 20:08:32 +0530

    Add files via upload

---
+++ FractionalNumber.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        fact = 1
+        for i in range(1, n+1):
+            fact = fact * i
+        return fact
+
+num = int(input("Enter a number: "))
+print("Factorial of", num, "is", factorial(num))
